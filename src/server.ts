54f2580947f542f468f8c1c7e6436f034f73db3c
emlemmon <77080516+emlemmon@users.noreply.github.com>
2026-01-30 14:46:11 -0700

    configured vite to use React and express, connected to MongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,26 @@
-import { createServer } from "http";
-import { handler } from "./handler.js";
+import express from "express";
+import ViteExpress from "vite-express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
 
-const port = 3000;
+const app = express();
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const mongoURI = process.env.MONGO_URI;
 
-const server = createServer(handler);
-server.listen(port, () => console.log(`Server listening on port ${port}`));
+app.get("/message", (_, res) => res.send("Hello from express!"));
+
+if (!mongoURI) {
+  console.error('Missing MongoDB connection string in environment variables');
+  process.exit(1);
+}
+
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    ViteExpress.listen(app, PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
